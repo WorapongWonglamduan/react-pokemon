55496a0e7d3814867f2bdc3ec1ba8ebbc6469ef5
WorapongWonglamduan <the_oo_ooo@hotmail.com>
2024-07-27 20:00:32 +0700

    card

--- src/components/card/CardItems.jsx
+++ src/components/card/CardItems.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import "./CardItems.css";
 
 const CardItems = ({ item }) => {
-  console.log("====================================");
-  console.log("item->", item);
-  console.log("====================================");
   const { name } = item;
-  return <div>CardItems {name} </div>;
+  return (
+    <div className="card-item">
+      <img
+        src="https://w7.pngwing.com/pngs/262/723/png-transparent-pokemon-charizard-illustration-pokemon-trading-card-game-pokemon-tcg-online-pokemon-box-ruby-sapphire-charizard-pokemon-mammal-dragon-orange.png"
+        alt={name}
+        className="card-item__img-pokemon"
+      />
+
+      <div className="card-item__details">
+        <div> Details : {name}</div>
+        <div> Details : {name}</div>
+        <div> Details : {name}</div>
+        <div> Details : {name}</div>
+      </div>
+    </div>
+  );
 };
 
 export default CardItems;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,11 +3,11 @@ import "./Home.css";
 import FilterComponent from "../filter/FilterComponent";
 import CardItems from "../card/CardItems";
 const Content = () => {
-  const array = Array.from({ length: 15 }, (_, index) => ({ name: index + 1 }));
+  const array = Array.from({ length: 20 }, (_, index) => ({ name: index + 1 }));
   return (
     <div className="row">
       {array.map((item) => (
-        <div key={item} className="col-lg-2">
+        <div key={item} className="col-xl-2 col-md-3">
           <CardItems item={item} />
         </div>
       ))}
